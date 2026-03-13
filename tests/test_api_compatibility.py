49d3b9ae2de8cb32e2d1499bb49a0f96ee59a14d
MikhailMamonov <mamon201071@gmail.com>
2026-03-13 09:43:40 +0300

    [develop-5] - fix unit tests

--- tests/test_api_compatibility.py
+++ tests/test_api_compatibility.py
@@ -4,7 +4,7 @@ from ..main import WBProduct, AsyncWBParser
 
 @pytest.mark.asyncio
 async def test_current_wb_api_structure():
-    parser = AsyncWBParser()
+    parser = await AsyncWBParser.create()
     products = await parser.search_products_catalog("пальто", page=1)
 
     assert len(products)>0, "API вернуло 0 товаров.Возможно, изменились параметры запроса или URL"
